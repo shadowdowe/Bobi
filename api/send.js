e511f22987bbc5da3f7f512096607b7bc30d1bf4
shadowdowe <aeaoaoeap@gmail.com>
2025-07-18 19:17:08 +0500

    Update send.js

--- api/send.js
+++ api/send.js
@@ -3,32 +3,7 @@ export default async function handler(request, response) {
         return response.status(405).json({ error: 'Method Not Allowed' });
     }
 
-    const { message, apiKey } = request.body;
+    const { apiKey, ...data } = request.body;
     const expectedApiKey = process.env.MY_SECRET_API_KEY;
     const botToken = process.env.TELEGRAM_BOT_TOKEN;
-    const chatId = process.env.TELEGRAM_CHAT_ID;
-
-    if (!expectedApiKey || !botToken || !chatId) {
-        return response.status(500).json({ error: 'Server configuration fucked up' });
-    }
-
-    if (apiKey !== expectedApiKey) {
-        return response.status(401).json({ error: 'Unauthorized: Invalid API Key' });
-    }
-
-    if (!message) {
-        return response.status(400).json({ error: 'Message is required' });
-    }
-
-    try {
-        const telegramApiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
-        await fetch(telegramApiUrl, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ chat_id: chatId, text: message }),
-        });
-        return response.status(200).json({ status: 'ok' });
-    } catch (error) {
-        return response.status(500).json({ error: 'Failed to forward message' });
-    }
-}
+    const chatId = process.
